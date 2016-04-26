fe35d1e23b39ff069fcc00d3cd7b04e2a961cf8a
Lancy1996 <lancyeris@gmail.com>
2016-04-26 21:52:06 +0800

    all

---
+++ six_1_3.asm
@@ -0,0 +1,15 @@
+.386
+
+code    segment     use16   para   public   'code'
+        assume  cs:code
+start:  xor ax,ax
+        mov dx,ax
+	mov ds,0
+        cli
+        mov dx,ds:[10h*4]
+        mov ax,ds:2[10h*4]
+        sti
+        mov ah,4ch
+        int 21h
+code    ends
+        end     start
