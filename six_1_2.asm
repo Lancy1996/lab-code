670e8de824ddee0c04fa92353402f9b8c39ac465
Lancy1996 <Lancy1996@users.noreply.github.com>
2016-04-28 17:22:23 +0800

    Update six_1_2.asm

--- six_1_2.asm
+++ six_1_2.asm
@@ -9,16 +9,4 @@ start:	push cs
 	mov ah,4ch
 	int 21h
 code	ends
-=======
-.386
-code	segment use16 para public 'code'
-	assume	cs:code
-start:	push cs
-	push ds
-	mov ax,3510h
-	int 21h
-	mov ah,4ch
-	int 21h
-code	ends
->>>>>>> e3db146ea5334cf795d18d08e7f431a0029aa53e
 	end	start
